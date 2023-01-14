444d824deec2859533bd1fe3a85d7b1701bc3edc
John Hobbs <john@velvetcache.org>
2023-01-13 22:11:35 -0600

    Fix CHIP-8 screen implementation.

--- chip-8/chip8/screen.go
+++ chip-8/chip8/screen.go
@@ -10,15 +10,29 @@ import (
 // there are 32 rows
 type screen [256]uint8
 
-// todo: wrapping
 func (s *screen) Write(sprites []byte, x, y uint8) bool {
 	collision := false
+
+	n := x % 8
+	x1 := x / 8
+	x2 := x1 + 1
+	if x2 > 7 { // wrap around
+		x2 = 0
+	}
 
 	for i := uint8(0); i < uint8(len(sprites)); i++ {
 		if !collision && s[(y+i)*8+x]&sprites[i] != 0 {
 			collision = true
 		}
-		s[(y+i)*8+x] = s[(y+i)*8+x] ^ sprites[i]
+		
+		if n == 0 {
+			// byte aligned, we only deal with x1
+			s[(y+i)*8+x1] = s[(y+i)*8+x1] ^ sprites[i]
+		} else {
+			// not byte aligned, we do both
+			s[(y+i)*8+x1] = s[(y+i)*8+x1] ^ (sprites[i] >> n)
+			s[(y+i)*8+x2] = s[(y+i)*8+x2] ^ (sprites[i] << (8-n))
+		}
 	}
 
 	return collision
