b200974a4fd77449a4d067dff2b70cdaa241543b
John Hobbs <john@velvetcache.org>
2024-09-26 22:20:05 -0500

    Implement remaining assembler opcodes

--- chip-8/asm/encoder.go
+++ chip-8/asm/encoder.go
@@ -12,22 +12,85 @@ type encoding struct {
 	encoder  encoder
 }
 
+// TODO: Aliasing.  CLEAR -> CLS, RETURN -> RET
+
 var instructions = map[string][]encoding{
-	"CLEAR": {
+	"CLS": {
 		{
-			operands: []Operand{},
 			encoder: func([]string) ([]byte, error) {
 				return []byte{0x00, 0xE0}, nil
 			},
 		},
 	},
+	"RET": {
+		{
+			encoder: func([]string) ([]byte, error) {
+				return []byte{0x00, 0xEE}, nil
+			},
+		},
+	},
+	"JMP": {
+		addressEncoding(0x10),
+		// V0, addr - Bnnn
+	},
+	"CALL": {
+		addressEncoding(0x20),
+	},
+	"SE": {
+		registerByteEncoding(0x30),
+		twoRegisterEncoding(0x50, 0x00),
+	},
+	"SNE": {
+		registerByteEncoding(0x40),
+		twoRegisterEncoding(0x90, 0x00),
+	},
 	"LOAD": {
 		registerByteEncoding(0x60),
 		twoRegisterEncoding(0x80, 0x00),
+		{
+			operands: []Operand{I_REGISTER, ADDRESS},
+			encoder:  addressEncoder(0xA0),
+		},
+		{
+			operands: []Operand{REGISTER, DELAY_TIMER},
+			encoder:  oneRegisterEncoder(0xF0, 0x07),
+		},
+		{
+			operands: []Operand{REGISTER, KEYPRESS},
+			encoder:  oneRegisterEncoder(0xF0, 0x0A),
+		},
+		{
+			operands: []Operand{DELAY_TIMER, REGISTER},
+			encoder:  oneRegisterEncoderSkip(0xF0, 0x15),
+		},
+		{
+			operands: []Operand{SOUND_TIMER, REGISTER},
+			encoder:  oneRegisterEncoderSkip(0xF0, 0x18),
+		},
+		{
+			operands: []Operand{SPRITE, REGISTER},
+			encoder:  oneRegisterEncoderSkip(0xF0, 0x29),
+		},
+		{
+			operands: []Operand{BCD, REGISTER},
+			encoder:  oneRegisterEncoderSkip(0xF0, 0x33),
+		},
+		{
+			operands: []Operand{I_REGISTER, REGISTER},
+			encoder:  oneRegisterEncoderSkip(0xF0, 0x55),
+		},
+		{
+			operands: []Operand{REGISTER, I_REGISTER},
+			encoder:  oneRegisterEncoder(0xF0, 0x65),
+		},
 	},
 	"ADD": {
 		registerByteEncoding(0x70),
 		twoRegisterEncoding(0x80, 0x04),
+		{
+			operands: []Operand{I_REGISTER, REGISTER},
+			encoder:  oneRegisterEncoderSkip(0xF0, 0x1E),
+		},
 	},
 	"OR": {
 		twoRegisterEncoding(0x80, 0x01),
@@ -42,10 +105,25 @@ var instructions = map[string][]encoding{
 		twoRegisterEncoding(0x80, 0x05),
 	},
 	"SHR": {
-		twoRegisterEncoding(0x80, 0x06),
+		oneRegisterEncoding(0x80, 0x06),
+	},
+	"SUBN": {
+		twoRegisterEncoding(0x80, 0x07),
 	},
 	"SHL": {
-		twoRegisterEncoding(0x80, 0x0E),
+		oneRegisterEncoding(0x80, 0x0E),
+	},
+	"RAND": {
+		registerByteEncoding(0xC0),
+	},
+	"DRAW": {
+		twoRegisterNibbleEncoding(0xD0),
+	},
+	"SKIP": {
+		oneRegisterEncoding(0xE0, 0x9E),
+	},
+	"SKIPN": {
+		oneRegisterEncoding(0xE0, 0xA1),
 	},
 }
 
@@ -73,9 +151,41 @@ func Encode(instruction string, operands []string) ([]byte, error) {
 }
 
 /*
+Create an encoder for the one register pattern, with a prefix and a suffix.
+The upper bits of the prefix will be used, and the lower bits of the suffix
+The encoder will parse registers and return 0xPXYS
+*/
+func oneRegisterEncoder(prefix, suffix uint8) encoder {
+	return func(operands []string) ([]byte, error) {
+		target, err := register(operands[0])
+		if err != nil {
+			return nil, err
+		}
+		return []byte{(prefix & 0xF0) | target, suffix & 0x0F}, nil
+	}
+}
+
+func oneRegisterEncoderSkip(prefix, suffix uint8) encoder {
+	return func(operands []string) ([]byte, error) {
+		target, err := register(operands[1])
+		if err != nil {
+			return nil, err
+		}
+		return []byte{(prefix & 0xF0) | target, suffix & 0x0F}, nil
+	}
+}
+
+func oneRegisterEncoding(prefix, suffix uint8) encoding {
+	return encoding{
+		operands: []Operand{REGISTER},
+		encoder:  oneRegisterEncoder(prefix, suffix),
+	}
+}
+
+/*
 Create an encoder for the two register pattern, with a prefix and a suffix.
 The upper bits of the prefix will be used, and the lower bits of the suffix
-The encoder will parse registers and return 0xPX 0xYS
+The encoder will parse registers and return 0xPXYS
 */
 func twoRegisterEncoder(prefix, suffix uint8) encoder {
 	return func(operands []string) ([]byte, error) {
@@ -87,10 +197,17 @@ func twoRegisterEncoder(prefix, suffix uint8) encoder {
 	}
 }
 
+func twoRegisterEncoding(prefix, suffix uint8) encoding {
+	return encoding{
+		operands: []Operand{REGISTER, REGISTER},
+		encoder:  twoRegisterEncoder(prefix, suffix),
+	}
+}
+
 /*
 Create an encoder for the register and byte pattern, with a prefix.
 The upper bits of the prefix will be used.
-The encoder will parse a register and a byte, and return 0xPX 0xNN
+The encoder will parse a register and a byte, and return 0xPXNN
 */
 func registerByteEncoder(prefix uint8) encoder {
 	return func(operands []string) ([]byte, error) {
@@ -102,16 +219,52 @@ func registerByteEncoder(prefix uint8) encoder {
 	}
 }
 
-func twoRegisterEncoding(prefix, suffix uint8) encoding {
-	return encoding{
-		operands: []Operand{REGISTER, REGISTER},
-		encoder:  twoRegisterEncoder(prefix, suffix),
-	}
-}
-
 func registerByteEncoding(prefix uint8) encoding {
 	return encoding{
 		operands: []Operand{REGISTER, BYTE},
 		encoder:  registerByteEncoder(prefix),
+	}
+}
+
+/*
+Create an encoder for the address pattern, with a prefix.
+The upper bits of the prefix will be used.
+The encoder will parse an address and return 0xPNNN
+*/
+func addressEncoder(prefix uint8) encoder {
+	return func(operands []string) ([]byte, error) {
+		target, err := address(operands[0])
+		if err != nil {
+			return nil, err
+		}
+		return []byte{prefix | uint8(target>>8), uint8(target & 0xFF)}, nil
+	}
+}
+
+func addressEncoding(prefix uint8) encoding {
+	return encoding{
+		operands: []Operand{ADDRESS},
+		encoder:  addressEncoder(prefix),
+	}
+}
+
+func twoRegisterNibbleEncoder(prefix uint8) encoder {
+	return func(operands []string) ([]byte, error) {
+		dest, src, err := registers(operands[0], operands[1])
+		if err != nil {
+			return nil, err
+		}
+		nibble, err := value(operands[2])
+		if err != nil {
+			return nil, err
+		}
+		return []byte{(prefix & 0xF0) | dest, src<<4 | (nibble & 0x0F)}, nil
+	}
+}
+
+func twoRegisterNibbleEncoding(prefix uint8) encoding {
+	return encoding{
+		operands: []Operand{REGISTER, REGISTER, BYTE},
+		encoder:  twoRegisterNibbleEncoder(prefix),
 	}
 }
--- chip-8/asm/operand.go
+++ chip-8/asm/operand.go
@@ -40,24 +40,72 @@ func registerValue(a, b string) (uint8, uint8, error) {
 	if err != nil {
 		return 0, 0, err
 	}
-	var value uint8
-	_, err = fmt.Sscanf(b, "%x", &value)
+	v, err := value(b)
+	return ar, v, err
+}
+
+/**
+ * Read a byte value from a hex encoded string.
+ * Returns an error if improperly formed.
+ */
+func value(a string) (uint8, error) {
+	var v uint8
+	_, err := fmt.Sscanf(a, "%x", &v)
 	if err != nil {
-		return 0, 0, fmt.Errorf("invalid value %q", b)
+		return 0, fmt.Errorf("invalid value %q", a)
 	}
-	return ar, value, nil
+	return v, nil
+}
+
+/**
+ * Read an address value from a string.
+ * Returns an error if out of bounds or improperly formed.
+ */
+func address(v string) (uint16, error) {
+	var n uint16
+	_, err := fmt.Sscanf(v, "%x", &n)
+	if err != nil || n > 4095 {
+		return 0, fmt.Errorf("invalid address %q", v)
+	}
+	return n, err
 }
 
 type Operand uint8
 
 var (
-	REGISTER Operand = 0
-	BYTE     Operand = 1
+	REGISTER    Operand = 0
+	BYTE        Operand = 1
+	ADDRESS     Operand = 2
+	DELAY_TIMER Operand = 3
+	SOUND_TIMER Operand = 4
+	I_REGISTER  Operand = 5
+	BCD         Operand = 6
+	KEYPRESS    Operand = 7
+	SPRITE      Operand = 8
 )
 
 func operandType(s string) Operand {
 	if s[0] == 'V' {
 		return REGISTER
+	}
+
+	switch s {
+	case "DT":
+		return DELAY_TIMER
+	case "ST":
+		return SOUND_TIMER
+	case "I":
+		return I_REGISTER
+	case "B":
+		return BCD
+	case "K":
+		return KEYPRESS
+	case "F": // TODO: this will collide with 16 -> F
+		return SPRITE
+	}
+
+	if len(s) == 3 {
+		return ADDRESS
 	}
 	return BYTE
 }
